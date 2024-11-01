d24d7234ed75be9c6dc6e26b5b699cbc31331c1a
r0bin <r0bin_dood@pm.me>
2024-11-01 07:35:44 -0400

    simple web server in 1 pritnf()

--- henf/main.c
+++ henf/main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 #define s   socket
+#define d   dup2
 #define so  setsockopt
 #define bd  bind
 #define l   listen
 #define a   accept
+#define o   open
 #define r   read
 #define w   write
 #define c   close
-int s(),so(),bd(),l(),a(),r(),w(),c();
+int s(),d(),so(),bd(),l(),a(),r(),w(),c(),o();
 
 #define BSZ 1024
 
@@ -18,21 +20,26 @@ int s(),so(),bd(),l(),a(),r(),w(),c();
 
 #define S       (b+1)
 #define SE      (b+5)
-#define SADDR   (b+6)
+#define SE2     (b+9)
+#define SADDR   (b+10)
 #define SADDRP  0x1f,0x90  // port: 8080
 #define SADDRA  0x7f,0,0,1 // addr: 127.0.0.1
 #define SADDRS  SADDRP,SADDRA
 #define SADDRSZ 16
 #define SARGS   2,1,6
-#define SOARGS  1,15,SE,1
+#define SOARGS  1,2,SE2,1
+#define SOARGS2 1,15,SE2,1
 #define SBUFSZ  512
 #define SBUF    (b+(BSZ-SBUFSZ))
 #define SA      (SADDR+SADDRSZ)
-#define RDSZ    (SADDR+SADDRSZ+4)
+#define WRSTR   (SADDR+SADDRSZ+4)
+#define WRSTRSZ 20
 
-static volatile char b[BSZ] = {0, 0,0,0,0, 1, 2,0,SADDRS,0,0,0,0,0,0,0,0 };
+#define SECHECK(x) (*AIP(SE)<=6?x:-1)
 
-int main(int argc, char **argv) {
-    while(!*EXIT) printf("%1$.*2$d%3$hn\r\033[K%5$s",0,s(SARGS),AIP(S),r(a(*AIP(S),0,l(*AIP(S),2-bd(*AIP(S),SADDR,SADDRSZ-so(*AIP(S),SOARGS)))?0:0),SBUF,SBUFSZ),SBUF);
+static volatile char b[BSZ] = {0, 2,0,0,0, 1,0,0,0, 1, 2,0,SADDRS,0,0,0,0,0,0,0,0, 4,0,0,0, 'H','T','T','P','/','1','.','1',' ','2','0','0',' ','O','k','\r','\n','\r','\n', '-'}; 
+
+int main(int ac, char **av) {
+    while(!*EXIT) printf("%2$.38s...\n%4$s\n%3$hhn",c(w(*AIP(SA),WRSTR,r(d(a(*AIP(S),0,l(SECHECK(*AIP(S)),bd(SECHECK(*AIP(S)),SADDR,so(d(SECHECK(s(SARGS)),*AIP(S)),SOARGS)?SADDRSZ:SADDRSZ)?1:1)?0:0),*AIP(SA)),SBUF,SBUFSZ-1)?r(o(av[1],0),WRSTR+(WRSTRSZ-1),256)+WRSTRSZ:WRSTRSZ)?*AIP(SA):*AIP(SA)),SBUF,AIP(SE),WRSTR);
     return 0;
 }
